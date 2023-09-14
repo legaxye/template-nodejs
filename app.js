38dcc5578f8bd1ab46b04073a03b3fb436349a0f
legaxye <81601255+legaxye@users.noreply.github.com>
2023-09-14 09:32:09 -0500

    prueba
    Signed-off-by: legaxye <81601255+legaxye@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+let contador = 0;
+
+function incrementarContador() {
+    contador += 100;
+}
+
+// Llamamos a setInterval fuera de la función para que se ejecute una vez cada 10 segundos.
+setInterval(() => {
+    contador += 10;
+}, 1000); // Incrementa el contador cada 10 segundos
+
+app.get('/', (req, res) => {
+    res.send('<p>Hello, World, me llamou jusemarias10!</p>');
+});
+
+app.get('/obtener_contador', (req, res) => {
+    res.json({ contador });
+});
+
+incrementarContador(); // Inicia el incremento del contador
+
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
